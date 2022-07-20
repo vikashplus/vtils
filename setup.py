1029b5cb2da54e484e43f691d2af521f14ac065d
Vikash Kumar <vikashplus@gmail.com>
2022-07-20 10:20:24 -0400

    Adding setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,30 @@
+""" =================================================
+Copyright (C) 2018 Vikash Kumar
+Author  :: Vikash Kumar (vikashplus@gmail.com)
+Source  :: https://github.com/vikashplus/vtils
+License :: Under Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+================================================= """
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info.major != 3:
+    print("This Python is only compatible with Python 3, but you are running "
+          "Python {}. The installation will likely fail.".format(sys.version_info.major))
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='vtils',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Utility scripts',
+    long_description=read('README.md'),
+    url='https://github.com/vikashplus/vtils.git',
+    author='Vikash Kumar',
+    install_requires=[
+         'matplotlib', 'pynput'
+    ],
+)

eaae9c6832f5859e852b96e85176de10b0a75121
dosssman <dosssman@hotmail.fr>
2024-01-29 17:04:09 +0900

    Added support for Xbox 360 controller for tele operation

--- vtils/input/gamepad.py
+++ vtils/input/gamepad.py
@@ -6,7 +6,11 @@ monitor_events = [  'ABS_X', 'ABS_Y',
                     'ABS_HAT0X', 'ABS_HAT0Y',
                     'ABS_RY', 'ABS_RX',
                     'BTN_EAST', 'BTN_WEST', 'BTN_NORTH', 'BTN_SOUTH',
-                    'BTN_TL', 'BTN_TR']
+                    'BTN_TL', 'BTN_TR',
+                    'BTN_START', 'BTN_SELECT',
+                    'BTN_THUMBL', 'BTN_THUMBR',
+                    'BTN_MODE', # Home (XBox) menu button
+                    "ABS_Z", 'ABS_RZ'] # NOTE: might be specific to Xbox 360 analog triggers
 
 class GamePad():
     """
@@ -17,7 +21,7 @@ class GamePad():
     def __init__(self):
         if self._GAMEPAD_CLIENT is None:
             for device in devices:
-                if device.name == "Logitech Gamepad F710":
+                if device.name in ["Logitech Gamepad F710", "Microsoft X-Box 360 pad"]:
                     self._GAMEPAD_CLIENT = device
                     print("Gamepad {} found".format(device))
             if self._GAMEPAD_CLIENT is None:
@@ -52,14 +56,17 @@ class GamePad():
 
     def read_sensor(self):
         events = self._GAMEPAD_CLIENT.read()
-
+        
+        # TODO: make the analog axis return normalized float for finer control ?
         for event in events:
             if event.code in monitor_events:
                 self.sensor_data['is_new'] = True # mark addition of new data
-                if event.code is 'ABS_RX' or event.code is 'ABS_RY':
-                    self.sensor_data[event.code] = event.state/32767
-                elif event.code is 'ABS_X' or event.code is 'ABS_Y':
-                    self.sensor_data[event.code] = int(event.state/32767)
+                if event.code == 'ABS_RX' or event.code == 'ABS_RY':
+                    self.sensor_data[event.code] = event.state/32767.0
+                elif event.code == 'ABS_X' or event.code == 'ABS_Y':
+                    self.sensor_data[event.code] = event.state/32767.0
+                elif event.code == 'ABS_RZ' or event.code == "ABS_Z":
+                    self.sensor_data[event.code] = event.state/255.0
                 else:
                     self.sensor_data[event.code] = event.state
             # else:

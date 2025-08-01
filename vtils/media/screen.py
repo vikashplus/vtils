7c11971649b80a1af0acf4fd862aafbf022b4bc1
Vikash Kumar <vikashplus@gmail.com>
2025-08-01 18:14:50 -0400

    BUG: Typecasting

--- vtils/media/screen.py
+++ vtils/media/screen.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def get_screen_dimensions(media_width, media_height, diagonal_size):
+
+def get_screen_dimensions(media_width: int, media_height: int, diagonal_size: float):
     """
     Calculate the screen width and height based on video resolution and screen diagonal size.
 
@@ -13,7 +14,7 @@ def get_screen_dimensions(media_width, media_height, diagonal_size):
         tuple: A tuple containing the screen width and height in meters.
     """
     # Calculate the aspect ratio
-    aspect_ratio = media_width / media_height
+    aspect_ratio = float(media_width) / float(media_height)
 
     # Calculate the screen height using the diagonal size and aspect ratio
     screen_height = diagonal_size / np.sqrt(1 + aspect_ratio**2)
